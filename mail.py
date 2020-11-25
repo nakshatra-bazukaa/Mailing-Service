2a471300c95393b944f8c5493e1e4840a29ef1d0
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-11-25 19:27:28 +0530

    Updated format

--- mail.py
+++ mail.py
@@ -1,3 +1,6 @@
+all_sid = [] #Put all the id in this list to which you want to send email
+message = "mail_message"
+
 import smtplib 
 import csv
 
@@ -8,11 +11,8 @@ with open(mecha, 'r') as data:
 	for line in csv.reader(data):
 		students[line[0]] = line[1]
 
-all_sid = [] #Put all the id in this list to which you want to send email
-
 sender_email = "sender_email_address" 
 sender_pwd = "sender_email_address_password" 
-message = "mail_message"
 
 for sid in all_sid: 
 	s = smtplib.SMTP('smtp.gmail.com', 587) 
@@ -20,3 +20,5 @@ for sid in all_sid:
 	s.login(sender_email, sender_pwd) 
 	s.sendmail(sender_email, students[sid], message) 
 	s.quit() 
+
+
